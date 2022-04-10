8db7faec5025396a80593a5e2797e84c5e740099
RopaBevy <rs2@wellesley.edu>
2022-04-10 19:11:47 -0400

    worked routes, queries and templates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import (Flask, render_template, make_response,
                    request, redirect, url_for, flash)
+import cs304dbi as dbi
 import bcrypt
 import queries
 
@@ -11,52 +12,62 @@ def get_login():
     return render_template("index.html")
 
 
-@app.route('/signup/', methods=['POST'])
+@app.route('/signup/', methods=['GET', 'POST'])
 def signup():
-    conn = dbi.connect()
-    # code to validate and add user to database goes here
-    email = request.form.get('email')
-    name = request.form.get('name')
-    password = request.form.get('password')
-    password2 = request.form.get('password2')
-    if password != password2:
-        flash('passwords do not match')
-        return redirect( url_for('signup'))
-
-    # if user is already in database, redirect back to signup page
-    if (queries.user_exists(conn, email)): 
-        return redirect(url_for('signup'))
+    if(request.method == 'GET'):
+        return render_template("signup.html")
     else:
-        # create a new user with the form data.
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        stored_password = hashed.decode('utf-8')
+        conn = dbi.connect()
+        # code to validate and add user to database goes here
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+        user_type = request.form.get('type')
 
-        # add the new user to the database
-        queries.insert_member(conn, email, password, name)
-    return redirect(url_for('login'))
+        #add an if statement that varifies that email as a wellesley email
+        if (password != password2):
+            flash('passwords do not match')
+            return redirect( url_for('signup'))
 
-@app.route('/login/', methods=['POST'])
+        # if user is already in database, redirect back to login page
+        if (queries.user_exists(conn, email)):
+            flash('An account with this email already exists. Please login.')
+            return redirect(url_for('login'))
+        else:
+            # create a new user with the form data.
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'),
+                                            bcrypt.gensalt())
+            stored_password = hashed_password.decode('utf-8')
+            # add the new user to the database
+            queries.insert_member(conn, email, stored_password, name, user_type)
+        return redirect(url_for('login'))
+
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
-    conn = dbi.connect()
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    login_result = queries.login(conn, email)
-    if(login_result is None):
-        flash('Login is incorrect. Please try again or sign up.')
-        return redirect( url_for('login'))
-
-    stored_password = login_result['password']
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'),
-                            stored_password.encode('utf-8'))
-                        
-    if(hashed_password = stored_password):
-        flash('Successfully logged in.')
-        #redirect them to the page for logged in people
-        return redirect('user', username=username) 
+    if(request.method == 'GET'):
+        return render_template("login.html")
     else:
-        flash('Login unsuccessful. Please try again or sign up.')
-        return redirect(url_for('get_login'))
+        conn = dbi.connect()
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        login_result = queries.login(conn, email)
+        if(login_result is None):
+            flash('Login is incorrect. Please try again or sign up.')
+            return redirect( url_for('login'))
+
+        stored_password = login_result['password']
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'),
+                                stored_password.encode('utf-8'))
+                            
+        if(hashed_password == stored_password):
+            flash('Successfully logged in.')
+            #redirect them to the page for logged in people
+            return redirect('user', username=username) 
+        else:
+            flash('Login unsuccessful. Please try again or sign up.')
+            return redirect(url_for('get_login'))
 
 
 
